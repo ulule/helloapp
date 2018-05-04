d44df3097c27cb714a89482a246bf1d1b5070cdf
Florent Messa <florent.messa@gmail.com>
2018-05-04 17:14:19 +0200

    feat: add more healthcheck endpoints

--- server.go
+++ server.go
@@ -13,6 +13,9 @@ import (
 
 func Run(port int) error {
 	r := chi.NewRouter()
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		render.Status(r, http.StatusOK)
+	})
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		render.Status(r, http.StatusOK)
 	})
