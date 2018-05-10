1b2910bbb2fc2b03e2bc793aebe40c0c7f79d062
Florent Messa <florent.messa@gmail.com>
2018-05-10 18:43:52 +0200

    feat: use same handler everywhere

--- server.go
+++ server.go
@@ -11,38 +11,36 @@ import (
 	"github.com/go-chi/render"
 )
 
-func Run(port int) error {
-	r := chi.NewRouter()
-	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		render.Status(r, http.StatusOK)
-	})
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		render.Status(r, http.StatusOK)
-	})
-	r.Get("/sys/health", func(w http.ResponseWriter, r *http.Request) {
-		headers := map[string]string{}
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	headers := map[string]string{}
 
-		for k, v := range r.Header {
-			if k == "Cookie" {
-				continue
-			}
-
-			headers[k] = v[0]
+	for k, v := range r.Header {
+		if k == "Cookie" {
+			continue
 		}
 
-		host, _ := os.Hostname()
+		headers[k] = v[0]
+	}
 
-		render.DefaultResponder(w, r, render.M{
-			"remote_addr": r.RemoteAddr,
-			"host":        host,
-			"headers":     headers,
-			"status":      "OK",
-			"version":     Version,
-			"revision":    Revision,
-			"build_time":  BuildTime,
-			"compiler":    Compiler,
-		})
+	host, _ := os.Hostname()
+
+	render.DefaultResponder(w, r, render.M{
+		"remote_addr": r.RemoteAddr,
+		"host":        host,
+		"headers":     headers,
+		"status":      "OK",
+		"version":     Version,
+		"revision":    Revision,
+		"build_time":  BuildTime,
+		"compiler":    Compiler,
 	})
+}
+
+func Run(port int) error {
+	r := chi.NewRouter()
+	r.Get("/healthz", healthcheck)
+	r.Get("/", healthcheck)
+	r.Get("/sys/health", healthcheck)
 
 	addr := fmt.Sprintf(":%s", strconv.Itoa(port))
 
