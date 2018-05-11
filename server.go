0e3ba9dc1c4148c6fc8b01e6e0982fbda92dcf27
Florent Messa <florent.messa@gmail.com>
2018-05-11 14:23:46 +0200

    feat: chi valve for proper shutdown

--- server.go
+++ server.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/valve"
 )
 
 func Run(port int) error {
@@ -17,12 +21,42 @@ func Run(port int) error {
 	r.Get("/", healthcheck)
 	r.Get("/sys/health", healthcheck)
 
+	valv := valve.New()
+	baseCtx := valv.Context()
+
 	addr := fmt.Sprintf(":%s", strconv.Itoa(port))
 
 	srv := http.Server{
 		Addr:    addr,
-		Handler: chi.ServerBaseContext(context.Background(), r),
+		Handler: chi.ServerBaseContext(baseCtx, r),
 	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			// sig is a ^C, handle it
+			fmt.Println("shutting down..")
+
+			// first valv
+			valv.Shutdown(20 * time.Second)
+
+			// create context with timeout
+			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+			defer cancel()
+
+			// start http shutdown
+			srv.Shutdown(ctx)
+
+			// verify, in worst case call cancel via defer
+			select {
+			case <-time.After(21 * time.Second):
+				fmt.Println("not all connections done")
+			case <-ctx.Done():
+
+			}
+		}
+	}()
 
 	return srv.ListenAndServe()
 }
