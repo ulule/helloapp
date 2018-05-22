fbfb211101da176139a093e9ecd665b6b099df8b
Florent Messa <florent.messa@gmail.com>
2018-05-22 21:28:31 +0200

    fix: pass gcloud project id as envvar in publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,5 @@ source scripts/version.sh
 
 make docker-build
 
-docker build -t gcr.io/ulule-services/helloapp:${HELLOAPP_VERSION} .
-gcloud docker -- push gcr.io/ulule-services/helloapp:${HELLOAPP_VERSION}
+docker build -t gcr.io/${GCLOUD_PROJECT_ID}/helloapp:${HELLOAPP_VERSION} .
+gcloud docker -- push gcr.io/${GCLOUD_PROJECT_ID}/helloapp:${HELLOAPP_VERSION}
