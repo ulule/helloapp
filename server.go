dd52f495fd8c42c2ef8f13f89771663038d50c45
Florent Messa <florent.messa@gmail.com>
2018-05-22 17:16:42 +0200

    ref: use TV4/graceful instead of valve

--- server.go
+++ server.go
@@ -4,15 +4,23 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"os"
-	"os/signal"
 	"strconv"
-	"time"
+
+	"github.com/TV4/graceful"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/valve"
 )
+
+type server struct {
+	http.Server
+}
+
+func (s *server) Shutdown(ctx context.Context) error {
+	fmt.Println("Finished shutdown")
+
+	return nil
+}
 
 func Run(port int) error {
 	r := chi.NewRouter()
@@ -21,42 +29,14 @@ func Run(port int) error {
 	r.Get("/", healthcheck)
 	r.Get("/sys/health", healthcheck)
 
-	valv := valve.New()
-	baseCtx := valv.Context()
-
 	addr := fmt.Sprintf(":%s", strconv.Itoa(port))
 
 	srv := http.Server{
 		Addr:    addr,
-		Handler: chi.ServerBaseContext(baseCtx, r),
+		Handler: chi.ServerBaseContext(context.Background(), r),
 	}
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		for range c {
-			// sig is a ^C, handle it
-			fmt.Println("shutting down..")
+	graceful.LogListenAndServe(&server{srv})
 
-			// first valv
-			valv.Shutdown(20 * time.Second)
-
-			// create context with timeout
-			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-			defer cancel()
-
-			// start http shutdown
-			srv.Shutdown(ctx)
-
-			// verify, in worst case call cancel via defer
-			select {
-			case <-time.After(21 * time.Second):
-				fmt.Println("not all connections done")
-			case <-ctx.Done():
-
-			}
-		}
-	}()
-
-	return srv.ListenAndServe()
+	return nil
 }
